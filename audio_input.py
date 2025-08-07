d49c7e9483206dd8fc4ec36be1ab46b5a6f99da8
Himanshu Dias <himanshudias2004@gmail.com>
2025-08-07 20:50:06 +0100

    audio stream started

--- audio_input.py
+++ audio_input.py
@@ -1,23 +1,39 @@
 import numpy as np
 import sounddevice as sd
 
+def audioCallback(indata , frames , time, status):
+    global audio_chunk
+    audio_chunk = indata[:,0]
+    print(np.sqrt(np.mean(audio_chunk**2)))
+
+def test():
+    print("TESTING FUNCT2")
 
 if __name__ == "__main__":
+
+    # Check devices
+    print(sd.default.device)
+
+
     duration = 5 #seconds
-    sd.default.samplerate =44100
+    # low sample rate for testing, this takes 2028 samples per second, and each block is 1024 samples,
+    # so output is the average volume in each block, i.e two times a second
+    sd.default.samplerate =2028
     sd.default.channels = 1
+    sd.default.blocksize = 1024
+    sd.default.dtype = 'float32'
+    sd.default.latency = 'low'
     #first param is number of frames total
-    print("Recording strted")
-    rec = sd.rec(int(duration * sd.default.samplerate))
+    test()
+    stream = sd.InputStream(callback=audioCallback)
 
-    sd.wait()
-    print("recording done")
+    print("Audio Stream Started")
+    stream.start()
+
+    sd.sleep(20000)
+
+    stream.stop()
+    print("Audio Stream Ended")
 
 
-    if np.all(rec == 0):
-        print("Silenve, detected")
-
-    print("palying back")
-    sd.play(rec)
-    sd.wait()
-    print("playback complete")
+    print(audio_chunk)

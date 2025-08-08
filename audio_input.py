e5c9434a1404db456211001d08039b3004d528db
Himanshu Dias <himanshudias2004@gmail.com>
2025-08-08 18:02:00 +0100

    simple circular buffer implemented

--- audio_input.py
+++ audio_input.py
@@ -1,14 +1,22 @@
 import numpy as np
 import sounddevice as sd
+from collections import deque
+
 
 def audioCallback(indata , frames , time, status):
     global audio_chunk
     audio_chunk = indata[:,0]
     print(np.sqrt(np.mean(audio_chunk**2)))
+    stream_buffer.addSamples(audio_chunk)
 
-def test():
-    print("TESTING FUNCT2")
+class AudioBuffer:
+    def __init__(self,max_size : int):
+        self.buffer = deque(maxlen=max_size)
+    
+    def addSamples(self, new_samples):
+        self.buffer.extend(new_samples)
 
+ 
 if __name__ == "__main__":
 
     # Check devices
@@ -18,22 +26,24 @@ if __name__ == "__main__":
     duration = 5 #seconds
     # low sample rate for testing, this takes 2028 samples per second, and each block is 1024 samples,
     # so output is the average volume in each block, i.e two times a second
-    sd.default.samplerate =2028
+
+    #SAMPLE RATE (ADJUST AS NEEDED), set to 44100Hz
+    sd.default.samplerate =44100
     sd.default.channels = 1
-    sd.default.blocksize = 1024
+    #BLOCKSIZE (ADJUST AS NEEDED), set to 256, gives 174 blocks per second
+    sd.default.blocksize = 256
     sd.default.dtype = 'float32'
     sd.default.latency = 'low'
     #first param is number of frames total
-    test()
-    stream = sd.InputStream(callback=audioCallback)
+    stream = sd.InputStream(callback=audioCallback)   
+
+    #BUFFER SIZE (ADJUST AD NEEDED), set to 4x the block size, samples are added to buffer in blocks, buffer holds 4 blocks, each sample has a latency of 22.3ms
+    stream_buffer = AudioBuffer(max_size=1024) 
 
     print("Audio Stream Started")
     stream.start()
 
-    sd.sleep(20000)
+    sd.sleep(5000)
 
     stream.stop()
     print("Audio Stream Ended")
-
-
-    print(audio_chunk)

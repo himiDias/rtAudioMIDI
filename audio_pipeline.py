3b74b4387ea95f4e56408caa476d26344ba57021
Himanshu Dias <himanshudias2004@gmail.com>
2025-08-17 18:58:49 +0100

    pyqt waveform made

--- audio_pipeline.py
+++ audio_pipeline.py
@@ -3,6 +3,12 @@ import sounddevice as sd
 import threading
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import sys
+
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QTimer
+import pyqtgraph as pg
+
 
 
 import audio_input
@@ -11,12 +17,10 @@ import AudioBuffer
 
 
 
-def audio_proccesing(frame,stream_buffer : AudioBuffer):
-    plt.clf()
+def audio_proccesing(stream_buffer : AudioBuffer):
     amplitudes =stream_buffer.getSampleWindow()
     if amplitudes.size >0:
-        plt.plot(amplitudes)
-        plt.ylim(-1,1)
+        values_waveform.setData(amplitudes)
         
      
 
@@ -28,18 +32,24 @@ if __name__ == "__main__":
 
     IOThread = threading.Thread(target=audio_input.startCapture,args=(stream_buffer, stop_event))
 
-    #ProcessorThread = threading.Thread(target=audio_proccesing, args=(stream_buffer, stop_event))
-
     IOThread.start()
 
-    ani = FuncAnimation(plt.gcf(),audio_proccesing,fargs=(stream_buffer,), interval=25,cache_frame_data=False)
-
     
-    plt.tight_layout()
-    plt.show()
-    #ProcessorThread.start()
+    app = QApplication(sys.argv)
+    win_waveform = pg.GraphicsLayoutWidget(show = True, title = "Real-time Waveform")
+    plot_waveform = win_waveform.addPlot(title="Waveform")
+    values_waveform = plot_waveform.plot(pen="r")
+    plot_waveform.setYRange(-1,1)
+
+    timer = QTimer()
+    timer.timeout.connect(lambda : audio_proccesing(stream_buffer=stream_buffer))
+    #on timeout, audio processing is called. Timeout signal sent every 22ms
+    # this is because the latency of the samples inside the buffer is roughly 22.3ms
+    # so this ensures samples are processed at the same rate as samples entered into the buffer
+    timer.start(22)
+
+    sys.exit(app.exec_())
+    
     stop_event.set()
 
     IOThread.join()
-
-    #ProcessorThread.join()
